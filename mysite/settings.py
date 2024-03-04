50849959eda9808dfc0cb53c61d547e81b08996a
Evan Sorrell <esorrell2@ivytech.edu>
2024-03-03 19:21:37 -0500

    Worked on the 'homework' portions of the tutorials and added the domain 'level-17.net' to direct to 'lucasfan99.pythonanywhere.com'

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +124,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
